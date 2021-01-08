9e4ab4d0d6d0e11d75e3fa3b289d513bb25ab3d5
Brett Turing <bjsherman80@gmail.com>
2021-01-08 09:26:00 -0700

    create rough test

--- spec/features/user_can_visit_root_path_spec.rb
+++ spec/features/user_can_visit_root_path_spec.rb
@@ -3,9 +3,24 @@ require 'rails_helper'
 describe "User can visit root path" do
   it "to see a search form" do
     visit '/'
-
     expect(current_path).to eq(root_path)
     expect(page).to have_content('Welcome to Poetry and Emotion')
     expect(page).to have_selector('input')
   end
+
+  it "can enter name of author and see their poems" do
+    visit '/'
+    fill_in :author, with: "Emily"
+    click_on 'Get Poems'
+
+    within(first('.poem')) do
+    expect(page).to have_content("Happy")
+    expect(page).to have_css('.title')
+    expect(page).to have_css('.author')
+    expect(page).to have_css('.poem')
+    expect(page).to have_css('.tone')
+  end
+  end
+
+
 end
